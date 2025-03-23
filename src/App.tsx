ec02fb4cb512bbe180376017b4f81a0bdd3f3699
paulius-ta <paulius.tartilas@gmail.com>
2025-03-23 12:34:24 +0200

    Add initial config files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "styles/global.scss";
+import "styles/variables.scss";
+import styles from "App.module.scss";
+
+function App() {
+  return (
+    <>
+      <div className={styles.app}>hello</div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+  ],
+});
